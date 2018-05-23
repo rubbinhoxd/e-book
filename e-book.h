c0425081b03b58bb38f40b0ca225381d0ff91db8
mateus-caetano <39563288+mateus-caetano@users.noreply.github.com>
2018-05-23 12:59:12 -0300

    Update e-book.h

--- e-book.h
+++ e-book.h
@@ -23,9 +23,20 @@ typedef struct
 	int senha ;
 } adm ;
 
+//função para cadastro de livros 
 livro * cadastra_livros () ;
+
+//função para edição de livros
 void edita_livros (livro *) ;
+
+//função para cadastro de clientes
 cliente * cadastra_cliente () ;
+
+//função para edição de perfil de cliente
 void edita_perfil (cliente *) ;
+
+//função para adição de cliente administrador
 adm * adiciona_adm () ;
+
+//função para exclusão de cliente administrador 
 void exclui_adm () ;
