ec990cac74dc951dca1ee56481b96e86f3dabb43
mateus-caetano <39563288+mateus-caetano@users.noreply.github.com>
2018-05-23 13:26:54 -0300

    Add files via upload

---
+++ e_book.c
@@ -0,0 +1,66 @@
+#include "e-book.h"
+
+int main(int argc, char const *argv[])
+{
+	int op ;
+
+	printf("Selecione o modulo desejado:\n") ;
+	printf("1.Login\n") ;
+	printf("2.Cadastro de livros\n") ;
+	printf("3.Edicao de livros\n") ;
+	printf("4.Cadastro de cliente\n") ;
+	printf("5.Edicao de perfil\n") ;
+	printf("6.Adiciona administrador\n") ;
+	printf("7.Excluir administrador\n") ;
+
+	switch(op)
+	{
+		case 1:
+		{
+
+		}
+
+		case 2:
+		{
+
+			break ;
+		}
+
+		case 3:
+		{
+
+			break ;
+		}
+
+		case 4:
+		{
+			
+			break ;
+		}
+
+		case 5:
+		{
+
+			break ;
+		}
+
+		case 6:
+		{
+
+			break ;
+		}
+
+		case 7:
+		{
+			
+			break ;
+		}
+
+		default:
+		{
+			printf("Opcao invalida!\n") ;
+		}
+	}
+
+	return 0;
+}
